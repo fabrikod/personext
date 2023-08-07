bacf0123cc524ede184de410a91bb69e9b28a585
yusufayhan0 <yusufay.ay1@gmail.com>
2023-08-07 09:10:40 +0300

    test(index): Test4

--- utils/readFile.js
+++ utils/readFile.js
@@ -4,18 +4,28 @@ const yaml = require('js-yaml');
 const fetchData = () => {
   const delimiter = '---';
   const markdownContent = fs.readFileSync('./README.md', 'utf-8');
+  var log = ["aaaaaaaaaaaaaaa"];
   const startIndex = markdownContent.indexOf(delimiter) + delimiter.length;
+  log.push(startIndex)
   const endIndex = markdownContent.indexOf(delimiter, startIndex);
+  log.push(endIndex)
 
   if (startIndex === -1 || endIndex === -1) {
     throw new Error('Invalid Markdown file format');
   }
 
+
   const extractedContent = markdownContent.slice(startIndex, endIndex).trim();
+
+  log.push(extractedContent)
+
   const yamlData = yaml.load(extractedContent);
+
+  log.push(yamlData)
 
   return {
     data: {
+      log: log,
       user: {
         email: yamlData.email,
         name: yamlData.name,
