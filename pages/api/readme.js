4889b8e6dc56f3bea35d2a27344f750533335372
yusufayhan0 <yusufay.ay1@gmail.com>
2023-08-10 19:29:50 +0300

    test(api): Test24

--- pages/api/readme.js
+++ pages/api/readme.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const path = require('path');
 
 const fetchData = (file) => {
   console.log('filefilefilefil e', file)
-  const path = `${process.cwd()}${file}`
-  console.log('44444444444', path)
-  const markdownContent = fs.readFileSync(process.cwd() + '/data/user.md', 'utf-8');
-  // const markdownContent2 = fs.readFileSync(path, 'utf-8');
-  console.log('55555555555', path)
+  const markdownContent = fs.readFileSync(path.join(process.cwd(), file), 'utf-8');
   const yamlData = yaml.load(markdownContent);
 
   return yamlData
