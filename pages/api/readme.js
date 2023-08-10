b37d4158c8214594a271529cc383b7cbb4255176
yusufayhan0 <yusufay.ay1@gmail.com>
2023-08-10 18:36:48 +0300

    test(api): Test14

--- pages/api/readme.js
+++ pages/api/readme.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml');
 
 const fetchData = (file) => {
   console.log('filefilefilefile', file)
+  console.log('process.cwd() + file', process.cwd() + file)
   const markdownContent = fs.readFileSync(process.cwd() + file, 'utf-8');
   console.log('markdownContentmarkdownContent', markdownContent)
 
