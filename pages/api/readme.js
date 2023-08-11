ee2c585118d8cbf8f549280caa65bf4541c7049b
yusufayhan0 <yusufay.ay1@gmail.com>
2023-08-11 06:42:45 +0300

    test(readme): Test34

--- pages/api/readme.js
+++ pages/api/readme.js
@@ -2,14 +2,11 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 const fetchData = (filePath) => {
-  console.log('filefilefilefil e', filePath)
   const deger = '/data/user.md'
-  const deger2 = filePath
   if (filePath === deger) {
     console.log('888888888', true)
   }
-  const markdownContent = fs.readFileSync(`${process.cwd()}${deger2}`, 'utf-8');
-  // const m = fs.readFileSync(process.cwd() + file, 'utf-8');
+  const markdownContent = fs.readFileSync(`${process.cwd()}${filePath}`, 'utf-8');
   const yamlData = yaml.load(markdownContent);
 
   return yamlData
@@ -23,7 +20,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'file parameter is missing.' });
     }
 
-    const data = fetchData(file)
+    const data = fetchData('/data/user.md')
 
     res.status(200).json({
       data
