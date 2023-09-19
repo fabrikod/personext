d8b54f16f3c8801289629c0f35b815846835889c
Yusuf AYHAN <yusufay.ay1@gmail.com>
2023-09-19 14:59:07 +0300

    test

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
