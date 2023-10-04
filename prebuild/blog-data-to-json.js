1b28aa068d5c884ca73b4be56bd404f338ca1ddf
Yusuf AYHAN <yusufay.ay1@gmail.com>
2023-10-04 18:57:20 +0300

    test

---
+++ prebuild/blog-data-to-json.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const yaml = (content) => {
+  const yaml = require('js-yaml');
+  return yaml.load(content);
+}
+
+const getMetaDataBySortBlog = async () => {
+  const MAIN_MD_FILE_PATH = `${process.cwd()}/data`
+  const BLOG_FOLDER_PATH = `${MAIN_MD_FILE_PATH}/blogs`
+
+  const blogMetaDataArray = []
+  const files = await fs.readdir(BLOG_FOLDER_PATH);
+
+  for (const file of files) {
+    if (path.extname(file) === '.md') {
+      const mdFilePath = path.join(BLOG_FOLDER_PATH, file);
+      const fileStats = await fs.stat(mdFilePath);
+      const content = await fs.readFile(mdFilePath, 'utf8');
+
+      blogMetaDataArray.push({
+        file: file,
+        createdAt: fileStats.birthtime,
+        publishedAt: new Date(yaml(content).publishedAt)
+      })
+    }
+  }
+
+  blogMetaDataArray.sort((first, last) => last.publishedAt - first.publishedAt)
+
+  await fs.writeFile(`${process.cwd()}/data/blogs.json`, JSON.stringify(blogMetaDataArray, null, 2), (err) => {
+    if (err) {
+      console.error('JSON file write error:', err);
+      return;
+    }
+  });
+}
+
+getMetaDataBySortBlog()
