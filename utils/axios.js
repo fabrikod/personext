16ec7ea1cb30d0b1f4e6e6cfad376804f1933ab3
Yusuf AYHAN <yusufay.ay1@gmail.com>
2023-11-30 13:39:25 +0300

    api link updated

--- utils/axios.js
+++ utils/axios.js
@@ -1,24 +1,30 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? `http://localhost:3000/api` : 'http://localhost:3000/api',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? `/api`
+      : 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
 apiClient.interceptors.response.use(
-  (response) => response.data,
-  (error) => {
-    const { response } = error;
+  response => response.data,
+  error => {
+    const { response } = error
     if (response) {
-      console.error(`Request failed with status code ${response.status}:`, error);
-      throw error;
+      console.error(
+        `Request failed with status code ${response.status}:`,
+        error
+      )
+      throw error
     } else {
-      console.error('Network error:', error);
-      throw new Error('Network error occurred. Please try again later.');
+      console.error('Network error:', error)
+      throw new Error('Network error occurred. Please try again later.')
     }
   }
-);
+)
 
 export default apiClient
