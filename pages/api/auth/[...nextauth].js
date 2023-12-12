2f46ba2366d10a3c0d2e8843614f64dd8420c946
Yusuf AYHAN <yusufay.ay1@gmail.com>
2023-12-12 13:49:52 +0300

    github auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GitHubProvider from 'next-auth/providers/github'
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: '0072a9ca306d48c43112',
-      clientSecret: '8db42ab84f4010813326cdff98b970a6c3ef3559',
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 }
