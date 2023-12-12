e4e61f1629afb84e7729453b5ebf10bec329fddf
Yusuf AYHAN <yusufay.ay1@gmail.com>
2023-12-12 16:13:27 +0300

    github auth test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export const authOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      callbackUrl: 'https://www.abdullahonden.com/api/auth/callback/github',
+      callbackUrl: 'https://www.abdullahonden.com/api/auth/signin',
     }),
   ],
   secret: process.env.SECRET,
