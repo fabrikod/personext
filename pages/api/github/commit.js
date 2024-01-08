c925c1633ca6037e2c2797a8e054306d2e456a97
Yusuf AYHAN <yusufay.ay1@gmail.com>
2024-01-08 11:33:08 +0300

    test 9

--- pages/api/github/commit.js
+++ pages/api/github/commit.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
   console.log('222222222', files)
 
-  const files2 = await fs.readdir(path.join(`${process.cwd()}`, '/data/'))
+  const files2 = await fs.readdir(path.join(`${process.cwd()}`, 'data'))
   console.log('3333333333', files)
 
   res.status(200).json({
